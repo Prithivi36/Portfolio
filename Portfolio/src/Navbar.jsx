5c1d6752553067ab2150f382508f7324c535052f
Prithivi <prithivipappian@gmail.com>
2024-06-17 10:54:27 +0530

    Dependencies Added

---
+++ Portfolio/src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <div className='bg-dark p-2'>
+            <button className="btn btn-dark">
+                <i className="bi bi-layout-sidebar fw-bolder"></i>
+            </button>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
