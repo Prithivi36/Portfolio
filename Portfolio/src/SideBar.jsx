ca913bec9afee66e06f6f266177a65470554de81
Prithivi <prithivipappian@gmail.com>
2024-06-20 07:17:30 +0530

    Sidebar Design Change

--- Portfolio/src/SideBar.jsx
+++ Portfolio/src/SideBar.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Info from './Info'
 
 function SideBar(props) {
+  const content=["Home","Experiences","Contact","Skills"]
+  const [active,setActive]=React.useState("Home")
+    const elements=content.map((i,j)=>{
+        return(
+            <p key={j} onClick={()=>setActive(i)} className={`${active===i?'nav-elements-active':'nav-elements'} rounded-2 py-1 px-2`}>{i}</p>
+        )
+    })
   return (
     <div className={`sidebar align-items-center justify-content-center d-flex bg-dark ${props.open?'sidebar-open   ':''}`}>
         <div className="elements d-flex flex-column px-3 gap-3">
-            <button className='btn btn-outline-light d-block rounded-2 py-1 px-2'>Home</button>
-            <button className='btn btn-outline-light d-block rounded-2 py-1 px-2'>Experience</button>
-            <button className='btn btn-outline-light d-block rounded-2 py-1 px-2'>Contact</button>
-            <button className='btn btn-outline-light d-block rounded-2 py-1 px-2'>Skills</button>
+            {...elements}
         </div>
     </div>
   )
