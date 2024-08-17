1051206f74f1e0f087664dc8c83a6e80050eaa00
PrakashKrishnanR <prakashkrishnan.official@gmail.com>
2024-08-17 19:12:10 +0530

    test commit

--- Portfolio/src/Experience/Timeline.jsx
+++ Portfolio/src/Experience/Timeline.jsx
@@ -4,7 +4,7 @@ import Info from '../Home/Info'
 function Timeline() {
   return (
     <>
-    <h3 className='text-center text-light'>Experience</h3>
+    <h3 className='text-center text-light'>My Works</h3>
       <div className="">
         <h4 className='ms-5 text-decoration-underline text-light mt-5'>Jenkins Test </h4>
       </div>
